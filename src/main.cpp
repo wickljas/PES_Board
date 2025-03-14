d6ad1f114006fbe63bdb2f64638c22252b9f02a8
wickljas <jasper.wickli@gmail.com>
2025-03-14 18:10:06 +0100

    asd

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,7 @@ int main()
 
     // led on nucleo board
     DigitalOut user_led(LED1);
+    DigitalIn RX(PA_0);
 
     // start timer
     main_task_timer.start();
@@ -41,15 +42,20 @@ int main()
     int buttonBefore = buttonNow;
 
     const float voltage_max = 12.0f; // maximum voltage of battery packs, adjust this to
-    const float gear_ratio_M2 = 156.0f; // gear ratio
-    const float kn_M2 = 89.0f / 12.0f;  // motor constant [rpm/V]
-    DCMotor motor_M1(PB_PWM_M1, PB_ENC_A_M1, PB_ENC_B_M1, gear_ratio_M2, kn_M2, voltage_max);
+    const float gear_ratio = 156.0f; // gear ratio
+    const float kn = 89.0f / 12.0f;  // motor constant [rpm/V]
+    DCMotor motor_M1(PB_PWM_M1, PB_ENC_A_M1, PB_ENC_B_M1, gear_ratio, kn, voltage_max);
+    DCMotor motor_M2(PB_PWM_M2, PB_ENC_A_M2, PB_ENC_B_M2, gear_ratio, kn, voltage_max);
+    DCMotor motor_M3(PB_PWM_M3, PB_ENC_A_M3, PB_ENC_B_M3, gear_ratio, kn, voltage_max);
 
     DigitalOut enable_motors(PB_ENABLE_DCMOTORS);
     enable_motors = 1;  
 
     motor_M1.setMaxVelocity(0.5f);
+    motor_M2.setMaxVelocity(0.5f);
+    motor_M3.setMaxVelocity(0.5f);
 
+    const float speed = 0.3f;
     // this loop will run forever
     while (true) {
         main_task_timer.reset();
@@ -58,6 +64,8 @@ int main()
             case STOPPING:
                 buttonNow = button.read();
                 motor_M1.setVelocity(0.0f);
+                motor_M2.setVelocity(0.0f);
+                motor_M3.setVelocity(0.0f);
                 user_led = 0;
 
                 if (buttonNow && !buttonBefore) {
@@ -69,7 +77,9 @@ int main()
             
             case MOVING:
                 buttonNow = button.read();
-                motor_M1.setVelocity(0.3f);
+                motor_M1.setVelocity(speed);
+                motor_M2.setVelocity(speed);
+                motor_M3.setVelocity(speed);
                 user_led = 1;
 
                 if (buttonNow && !buttonBefore) {
@@ -83,14 +93,11 @@ int main()
                 break;
 
         }
-        if (buttonNow && !buttonBefore) {
-            user_led = !user_led;
-            motor_M1.setVelocity(0.5f);
-        }
 
-        buttonBefore = buttonNow;
-        printf("Motor velocity: %f \n", motor_M1.getVelocity());
 
+    
+        //printf("Motor velocity: %f \n", motor_M1.getVelocity());
+        printf("ESP32 Button?: %d \n", RX.read());
         // read timer and make the main thread sleep for the remaining time span (non blocking)
         int main_task_elapsed_time_ms = duration_cast<milliseconds>(main_task_timer.elapsed_time()).count();
         if (main_task_period_ms - main_task_elapsed_time_ms < 0)
