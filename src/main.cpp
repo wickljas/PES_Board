69bdecdc585350a42cdc8a369663a32edac35e01
wickljas <jasper.wickli@gmail.com>
2025-03-14 16:58:59 +0100

    button

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,17 @@
 // drivers
 #include "DebounceIn.h"
 
-bool do_execute_main_task = false; // this variable will be toggled via the user button (blue button) and
-                                   // decides whether to execute the main task or not
+bool do_execute_main_task = false; // this variable will be toggled via the user button (blue button) and                                   // decides whether to execute the main task or not
 bool do_reset_all_once = false;    // this variable is used to reset certain variables and objects and
                                    // shows how you can run a code segment only once
 
 // objects for user button (blue button) handling on nucleo board
-DebounceIn user_button(BUTTON1);   // create DebounceIn to evaluate the user button
-void toggle_do_execute_main_fcn(); // custom function which is getting executed when user
-                                   // button gets pressed, definition below
+DigitalIn button(BUTTON1);
+
 
 // main runs as an own thread
 int main()
 {
-    // attach button fall function address to user button object
-    user_button.fall(&toggle_do_execute_main_fcn);
-
     // while loop gets executed every main_task_period_ms milliseconds, this is a
     // simple approach to repeatedly execute main
     const int main_task_period_ms = 20; // define main task period time in ms e.g. 20 ms, there for
@@ -32,34 +27,22 @@ int main()
     // led on nucleo board
     DigitalOut user_led(LED1);
 
-    // additional led
-    // create DigitalOut object to command extra led, you need to add an aditional resistor, e.g. 220...500 Ohm
-    // a led has an anode (+) and a cathode (-), the cathode needs to be connected to ground via the resistor
-    DigitalOut led1(PB_9);
-
     // start timer
     main_task_timer.start();
+
+    int buttonNow = button.read();
+    int buttonBefore = buttonNow;
 
     // this loop will run forever
     while (true) {
         main_task_timer.reset();
 
-        if (do_execute_main_task) {
-
-            // visual feedback that the main task is executed, setting this once would actually be enough
-            led1 = 1;
-        } else {
-            // the following code block gets executed only once
-            if (do_reset_all_once) {
-                do_reset_all_once = false;
-
-                // reset variables and objects
-                led1 = 0;
-            }
+        buttonNow = button.read();
+        if (buttonNow && !buttonBefore) {
+            user_led = !user_led;
         }
 
-        // toggling the user led
-        user_led = !user_led;
+        buttonBefore = buttonNow;
 
         // read timer and make the main thread sleep for the remaining time span (non blocking)
         int main_task_elapsed_time_ms = duration_cast<milliseconds>(main_task_timer.elapsed_time()).count();
@@ -70,11 +53,3 @@ int main()
     }
 }
 
-void toggle_do_execute_main_fcn()
-{
-    // toggle do_execute_main_task if the button was pressed
-    do_execute_main_task = !do_execute_main_task;
-    // set do_reset_all_once to true if do_execute_main_task changed from false to true
-    if (do_execute_main_task)
-        do_reset_all_once = true;
-}
